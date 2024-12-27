b5228b113fe8808d78cda29fd9b8429dddd5946d
NishikantDounekar <119045534+nishikant23@users.noreply.github.com>
2024-12-27 15:59:26 +0530

    Time: 0 ms (100.00%) | Memory: 51.3 MB (86.53%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.ts
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.ts
@@ -1,19 +1,19 @@
 function search(nums: number[], target: number): number {
     let n: number  = nums.length;
     let pivot : number = getPivot(nums, 0, n-1);
-    if(target == nums[0]) return 0;
-    if(target == nums[1]) return 1;
-    if(target == nums[n-1]) return n-1;
+    if(target == nums[0]) return 0; //early findings
+    if(target == nums[1]) return 1; //early findings
+    if(target == nums[n-1]) return n-1; //early findings
     if(pivot == -1) { //whole sorted array w/o rotaion
-        return getTarget(nums, 0, n-1, target);
-    }else{ //had rotation
-        if(target == nums[pivot]) return pivot;
-        else if((target < nums[0] && target>nums[n-1]) || target<nums[pivot]){
+        return getTarget(nums, 0, n-1, target); //like General Binary Search
+    }else{ //had rotation , then 1st find min-number index
+        if(target == nums[pivot]) return pivot; 
+        else if((target < nums[0] && target>nums[n-1]) || target<nums[pivot]){  // target not presen , by chking with min-numINDEX, oth-Indx & lastindex
             return -1;
-        }else if(target >= nums[pivot] && target<=nums[n-1]) return getTarget(nums, pivot, n-1, target);
-        else if(target >= nums[0] && target<=nums[pivot-1]) return getTarget(nums, 0, pivot-1, target );
+        }else if(target >= nums[pivot] && target<=nums[n-1]) return getTarget(nums, pivot, n-1, target); //target lies leftSide of pivot-min-num postion
+        else if(target >= nums[0] && target<=nums[pivot-1]) return getTarget(nums, 0, pivot-1, target ); //target lies rightSide of pivot-min-num postion
     } 
-    return -1;
+    return -1; //num , not present
 };
 
 const getPivot = (arr: number[], l: number, r: number) => {
